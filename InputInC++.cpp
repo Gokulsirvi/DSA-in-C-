590a1d9b8baa326f0879d34ade0314989ae3fda9
Gokulsirvi <gokulsirvi6@gmail.com>
2025-07-10 13:08:48 +0530

    Create InputInC++.cpp

---
+++ InputInC++.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#define size 100
+char stack[size];
+int top =-1; 
+
+int main() {
+    int n;
+    //for printf
+    std::cout << "Hello world\n"<<"enter a number"<<endl;
+    //for scanf
+    cin>>n;
+    return 0;
+}
